50474274fe2ed831d5d356d02767041e0484f7eb
Tymen <tymenschreuder@yahoo.com>
2009-09-01 22:00:00 -0600

    drat-darnit. Work, fucker

--- lib/R/grouper.R
+++ lib/R/grouper.R
@@ -1,4 +1,30 @@
-group <- function(coord_hash, num_groups) 
+#tmpArray = array(1:3, c(50,3))
+#Example Usage:
+# dfGrouped = group(mat_or_Table, numOfGroupsDesired)
+group <- function(coord_array, num_groups) 
 {
-	c(5);
+
+rowCount = length(coord_array)
+matConvert = matrix(0, ncol=3, nrow=rowCount)
+for(i in 1:rowCount)
+{
+  matConvert[i,]=unlist(coord_array[i])
+}
+
+ #mat_from_array = matrix(vects_data, ncol=3, nrow=8)
+ dfLatLong = data.frame(matConvert)
+  #Cluster analysis function is kmeans()
+  clusters = kmeans(dfLatLong, center=num_groups, nstart=5, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+# Available components:
+#[1] "cluster"  "centers"  "withinss" "size" 
+  
+  df_Grouping = data.frame(cbind(dfLatLong, clusters$cluster)) 
+  #vectGroups = df_Grouping[,4]
+  colnames(df_Grouping)=c("id","lat", "long", "grouping")#not used ultimately, but maybe later
+  #convert to matrix, and then array
+  matNew = data.matrix(df_Grouping)
+  array_Return = array(matNew, dim=c(rowCount,4))
+  #what we are returning is the following:
+  #[[1.0, 45.0, 100.0, 1.0], [2.0, 40.0, 100.0, 1.0], [3.0, 45.0, -100.0, 2.0], [4.0, 40.0, -100.0, 2.0], [5.0, -45.0, -100.0, 4.0], [6.0, -40.0, -100.0, 4.0], [7.0, -45.0, 100.0, 3.0], [8.0, -40.0, 100.0, 3.0]]
+
 }
--- spec/R/grouper_spec.rb
+++ spec/R/grouper_spec.rb
@@ -11,11 +11,12 @@ describe "Grouper" do
   it "should group coordinates into specified number of groups by nearest neighbors" do
     coords = [ [1,45,100],[2,40,100],[3,45,-100],[4,40,-100],[5,-45,-100],[6,-40,-100],[7,-45,100],[8,-40,100] ]
     grouped_coords = $rsruby.group(coords, 4)
-    grouped_coords[0][3].should == 1
-    grouped_coords[1][3].should == 1
-    grouped_coords[3][3].should == 2
-    grouped_coords[5][3].should == 3
-    grouped_coords[8][3].should == 4
+ p grouped_coords
+    grouped_coords[0][3].should == grouped_coords[1][3]
+    grouped_coords[2][3].should == grouped_coords[3][3]
+    grouped_coords[4][3].should == grouped_coords[5][3]
+    grouped_coords[6][3].should == grouped_coords[7][3]
+    
   end
   
 end
