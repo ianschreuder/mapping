0e9bb7459cdd0029266e682c0090f043cbf7d657
Ian Schreuder <ian@Josephine.local>
2009-10-02 14:01:26 -0600

    updated db

--- lib/load_csv.rb
+++ lib/load_csv.rb
@@ -10,18 +10,27 @@ def open_csv_file(filepath, &block)
   end
 end
 
-file = RAILS_ROOT + "/data/wind data.csv"
+file = RAILS_ROOT + "/data/turbine lat long.csv"
 
 contents = []
 open_csv_file(file){|row| contents << row }
 headers = contents.shift
 data = contents
 data.each do |line|
-  turbine = Turbine.find_by_label(line[3]) || Turbine.new(:label => line[3])
-  forecast = TurbineForecast.new(:iso_date => line[1], :hourly_ws => line[2], :created_at => line[4],
-                                 :verify_day => line[5], :verify_hour => line[6], :lead_time => line[7],
-                                 :forecast => line[8], :observed => line[9], :error => line[10])
-  forecast.turbine = turbine
-  turbine.save
-  forecast.save
+  turbine = Turbine.find_by_label(line[1])
+  if (turbine)
+    turbine.lat = line[2]
+    turbine.long = line[3]
+    turbine.save
+  else
+    p "could not find turbine : #{line[1]}"
+  end
+  
+  # turbine = Turbine.find_by_label(line[3]) || Turbine.new(:label => line[3])
+  # forecast = TurbineForecast.new(:iso_date => line[1], :hourly_ws => line[2], :created_at => line[4],
+  #                                :verify_day => line[5], :verify_hour => line[6], :lead_time => line[7],
+  #                                :forecast => line[8], :observed => line[9], :error => line[10])
+  # forecast.turbine = turbine
+  # turbine.save
+  # forecast.save
 end
